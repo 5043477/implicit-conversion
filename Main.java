7d35b213907f4b6b8a5f9f3974146054a8ccda8d
bwhitmore7 <bryson.whitmore@patelhs.org>
2021-03-03 22:59:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,39 @@
+class Main {
+  public static void main(String[] args) {
+    int i = 100;
+
+// automatic type conversion
+    long l = i;
+
+// automatic type conversion
+    float f = l;
+
+    // automatic type conversion
+    boolean b = true;
+
+    // automatic type conversion
+    double d = 100.5455;
+
+    // automatic type conversion
+    char c = ;
+
+    // automatic type conversion
+    short s = 5; 
+
+   // automatic type conversion
+   byte tb = 17;
+    System.out.println("Int or integer Value" + i);
+    System.out.println("Long Value " + l);
+    System.out.println("float value " + f);
+    System.out.println("boolean value " + b);
+    System.out.println("double value " + d);
+    System.out.println("char value " + c);
+    System.out.println("short value " + s);
+    System.out.println(" value " + tb);
+    
+
+
+
+
+  }
+}
