74d65ce03205d7447dfc363a713c7e17fe60867a
bwhitmore7 <bryson.whitmore@patelhs.org>
2021-03-05 22:00:32 +0000

    saving

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+   System.out.println("what is a number for the integer?"); 
+    int i = scan.nextInt();
+    System.out.println("what is the long variable range");
+    int l = scan.nextInt();
+    System.out.println("what is the float variable range");
+    int f = scan.nextInt();
+    System.out.println("what is the boolean");
+    String b = scan.next();
+    System.out.println("what is a double number");
+    int d = scan.nextInt();
+    System.out.println(" what is a char case");
+    String c = scan.next();
+    System.out.println("what is a short");
+    int s = scan.nextInt();
+    System.out.println(" what is a btye");
+    int tb = scan.nextInt();
+  }
+   { 
     int i = 100;
 
 // automatic type conversion
